5e2b642a6ae2856b31fc6c436d614c671b818072
marcozellweger <git@marcozellweger.ch>
2021-02-16 07:22:15 +0100

    create reviewable branch

---
+++ js/LocationFinder.js
@@ -0,0 +1,53 @@
+export default class LocationFinder extends HTMLElement {
+    constructor() {
+        super();
+        this.locationValue;
+        // get other components
+        this.locationSelect = this.querySelector('location-select');
+        this.locationButtons = this.querySelector('location-buttons');
+        this.locationAddress = this.querySelector('location-address');
+        
+    }
+
+    connectedCallback() {
+        this.setupLocationChangeListener();
+    }
+
+    setupLocationChangeListener() {
+        this.addEventListener('changelocation', (ev) => {
+            this.locationValue = ev.detail;
+
+            // change location address
+            this.changeAddress();
+            this.changeButtons();
+            this.changeSelect();
+        });
+    }
+
+    changeAddress() {
+        const addresses = this.locationAddress.querySelectorAll('address');
+        for (const address of addresses) {
+            address.dataset.location === this.locationValue ? this.selectElement(address) : this.deselectElement(address);
+        }
+    }
+
+    changeButtons() {
+        const buttons = this.locationButtons.querySelectorAll('button');
+        for (const button of buttons) {
+            button.value === this.locationValue ? this.selectElement(button) : this.deselectElement(button);
+        }
+    }
+
+    changeSelect() {
+        const select = this.locationSelect.querySelector('select');
+        requestAnimationFrame(() => select.value = this.locationValue);
+    }
+
+    selectElement(element) {
+        requestAnimationFrame(() => element.dataset.isSelected = true);
+    }
+
+    deselectElement(element) {
+        requestAnimationFrame(() => element.dataset.isSelected = false);
+    }
+}
